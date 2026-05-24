daf27f4c2f0abc97ab9e3d8b34ecb260967f50ac
Tong xuewen <111874120+yeahnangua@users.noreply.github.com>
2026-05-24 23:18:19 +0800

    feat: add github star link to page controls

--- Extension/Source/LyricViews/Page/Contained.ts
+++ Extension/Source/LyricViews/Page/Contained.ts
@@ -25,6 +25,11 @@ const Container = `
 const Header = `
 	<div class="ViewControls">
 		<button id="Romanize" class="ViewControl"></button>
+		<button id="Github" class="ViewControl">
+			<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 Svg-img-16-icon">
+				<path d="M8 0C3.58 0 0 3.58 0 8a8 8 0 0 0 5.47 7.59c.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82a7.55 7.55 0 0 1 4 0c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8 8 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+			</svg>
+		</button>
 		<button id="Cinema" class="ViewControl">
 			<svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16" data-encore-id="icon" class="Svg-sc-ytk21e-0 Svg-img-16-icon">
 				<path d="M14.55 1c.8 0 1.45.65 1.45 1.45V7h-1.5V2.5h-13v11h5.507V15H1.45C.65 15 0 14.35 0 13.55V2.45C0 1.65.65 1 1.45 1h13.1z"></path><path d="M16 9.757a.75.75 0 0 0-.75-.75H9.068L6.56 6.5h1.385a.75.75 0 1 0 0-1.5H4v3.946a.75.75 0 0 0 1.5 0V7.561l3.076 3.075v3.614c0 .414.336.75.75.75h5.925a.75.75 0 0 0 .75-.75V9.757z"></path>
@@ -75,6 +80,7 @@ export default class PageView implements Giveable {
 		// Handle our controls
 		{
 			// Grab our controls
+			const githubButton = header.querySelector<HTMLButtonElement>("#Github")!
 			const changeButton = header.querySelector<HTMLButtonElement>("#Cinema")!
 			const romanizeButton = header.querySelector<HTMLButtonElement>("#Romanize")!
 			const closeButton = header.querySelector<HTMLButtonElement>("#Close")!
@@ -93,6 +99,23 @@ export default class PageView implements Giveable {
 				closeButton.addEventListener(
 					"click",
 					() => this.Close()
+				)
+			}
+
+			// Handle our github button
+			{
+				const githubTooltip = Spotify.Tippy(
+					githubButton,
+					{
+						...Spotify.TippyProps,
+						content: "Star this project? It helps a lot. ❤️"
+					}
+				)
+				this.Maid.Give(() => githubTooltip.destroy())
+
+				githubButton.addEventListener(
+					"click",
+					() => globalThis.open("https://github.com/yeahnangua/beautiful-lyrics-reborn", "_blank", "noopener,noreferrer")
 				)
 			}
 
